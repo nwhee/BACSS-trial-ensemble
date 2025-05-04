27aea639c59144ebbf60a592d2bd70c571e85239
nwhee <ding.jo@northeastern.edu>
2025-05-03 23:12:17 -0700

    Analyze timestamp data for instagram and export to csv

---
+++ analyze.py
@@ -0,0 +1,57 @@
+import json
+import os
+import csv
+
+filt_instagram_dir = "ensemble_data/filtered_instagram"
+filt_tiktok_dir = "ensemble_data/filtered_tiktok"
+
+date_with_count_instagram = {}
+date_with_count_tiktok = {}
+
+def load_json(platform):
+    """
+    Loads JSON from files
+
+    Args: 
+        platform (str): The platform where the data comes from
+    """
+
+    directory = filt_instagram_dir if platform == "instagram" else filt_tiktok_dir
+
+    for filename in os.listdir(directory):
+        if ".json" in filename:
+            with open(f"{directory}/{filename}") as f:
+                data = json.load(f)
+                analyze_instagram(data, filename) if platform == "instagram" else analyze_tiktok(data, filename)
+
+    write_to_csv(platform)
+    
+
+def analyze_instagram(data, filename):
+    split_filename = filename.split("_")
+    date = split_filename[1]
+    # time = split_filename[2].split(".")[0]
+    count = data["data"]["count"]
+    if date in date_with_count_instagram:
+        date_with_count_instagram[date] += count
+    else:
+        date_with_count_instagram[date] = count
+    
+
+def analyze_tiktok(data, filename):
+    filename = ""
+
+
+def write_to_csv(platform):
+    csv_filename = f"date_with_count_{platform}.csv"
+
+    with open(csv_filename, mode = "w") as f:
+        writer = csv.writer(f)
+        if platform == "instagram":
+            for key, value in sorted(date_with_count_instagram.items()):
+                writer.writerow([key, value])
+        else:
+            for key, value in sorted(date_with_count_tiktok.items()):
+                writer.writerow([key, value])
+
+load_json("instagram")
